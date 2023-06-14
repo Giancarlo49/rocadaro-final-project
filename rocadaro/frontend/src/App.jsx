b0632076571213a82097f62bd722ceb62f5d8a62
David <davidcohnen@dg-email.de>
2023-06-14 12:01:17 +0200

    feertig diskutiert

--- rocadaro/frontend/src/App.jsx
+++ rocadaro/frontend/src/App.jsx
@@ -1,14 +1,33 @@
 import "./App.css";
 import DarkVariantExample from "./components/carousel/Carousel.jsx";
 import Navbar from "./components/navbar/Navbar.jsx";
-
+import ImgOverlayExample from "./components/footer/Footer.jsx";
+import GroupExample from "./components/brands/Brands.jsx";
+import VariationsExample from "./components/cards/Cards.jsx";
+import AnimationExample from "./components/footer/Footer4";
 // Pfade können je nach Ordnerstruktur variieren
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <br />
+      <br />
       <DarkVariantExample />
+      <br />
+      <br />
+      <ImgOverlayExample />
+      <br />
+      <GroupExample />
+      <br />
+      <br />
+      <VariationsExample />
+      <br />
+      <br />
+      <AnimationExample />
+      
+      
+
     </div>
   );
 };
---
+++ rocadaro/frontend/src/components/cards/Cards.jsx
@@ -0,0 +1,26 @@
+import Badge from 'react-bootstrap/Badge';
+
+function VariationsExample() {
+  return (
+    <div>
+      <Badge bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      <Badge bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      <Badge bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      <Badge bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      <Badge bg="light" text="dark">
+        Light
+      </Badge>{' '}
+      
+    </div>
+  );
+}
+
+export default VariationsExample;
