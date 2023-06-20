c70983ea3b9e87f93b095852eb0966e451303e5c
RoJiCal <roberto.jimenez@dci.education>
2023-06-20 14:07:55 +0200

    container brands

--- rocadaro/frontend/src/pages/Brands.jsx
+++ rocadaro/frontend/src/pages/Brands.jsx
@@ -1,10 +1,14 @@
 import "./Brands.css";
+import ContainerBrands from "../components/brands/ContainerBrands";
+
 
 const Brands = () => {
   return (
     <div className="page">
       <h1>Brands</h1>
       {/* Weitere Inhalte für Brands */}
+          <ContainerBrands/>
+          
     </div>
   );
 };
