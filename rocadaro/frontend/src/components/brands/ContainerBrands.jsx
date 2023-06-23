b0e6cd65e71832f66813306b185f316912dd5566
Giancarlo <giancarlo.scalisi@dci.education>
2023-06-23 15:20:13 +0200

    logos teils eingefügt

--- rocadaro/frontend/src/components/brands/ContainerBrands.jsx
+++ rocadaro/frontend/src/components/brands/ContainerBrands.jsx
@@ -72,66 +72,72 @@ function ContainerBrands() {
         <div className="row">
           <div className="box">
             <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+              <img src="./public/images/brands/adidas.jpeg" alt="Logo Adidas" />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.apple.com">
+              <img src="./public/images/brands/apple.jpg" alt="Logo Apple" />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.bosh.com">
+              <img src="./public/images/brands/bosh.jpg" alt="Logo Bosh" />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
-            </a>
-          </div>
-        </div>
-        <div className="row">
-          <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
-            </a>
-          </div>
-          <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
-            </a>
-          </div>
-          <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
-            </a>
-          </div>
-          <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.canon.com">
+              <img src="./public/images/brands/canon.jpg" alt="Logo Canon" />
             </a>
           </div>
         </div>
         <div className="row">
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.guess.com">
+              <img src="./public/images/brands/guess.png" alt="Logo Guess" />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.lacoste.com">
+              <img
+                src="./public/images/brands/lacoste.jpg"
+                alt="Logo Lacoste"
+              />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.lg.com">
+              <img src="./public/images/brands/LG.jpg" alt="Logo LG" />
             </a>
           </div>
           <div className="box">
-            <a href="https://www.adidas.com">
-              <img src="./src/images/brands/adidas.jpeg" alt="Logo" />
+            <a href="https://www.nike.com">
+              <img src="./public/images/brands/nike.jpg" alt="Logo Nike" />
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="box">
+            <a href="https://www.nikon.com">
+              <img src="./public/images/brands/nikon.jpg" alt="Logo Nikon" />
+            </a>
+          </div>
+          <div className="box">
+            <a href="https://www.puma.com">
+              <img src="./public/images/brands/puma.jpg" alt="Logo Puma" />
+            </a>
+          </div>
+          <div className="box">
+            <a href="https://www.samsung.com">
+              <img
+                src="./public/images/brands/samsung.png"
+                alt="Logo Samsung"
+              />
+            </a>
+          </div>
+          <div className="box">
+            <a href="https://www.still.com">
+              <img src="./public/images/brands/still.png" alt="Logo Still" />
             </a>
           </div>
         </div>
