2dd4ae5a7ce88243eccf3a37b8a1d467ced9113c
RobertoRadina <robertoradina@gmail.com>
2023-06-27 09:53:42 +0200

    navbar zurück

--- rocadaro/frontend/src/components/navbar/Navbar.jsx
+++ rocadaro/frontend/src/components/navbar/Navbar.jsx
@@ -3,8 +3,10 @@ import "./Navbar.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import ModalComponents from "../ModalComponents";
 // import Modal from "./src/modal/Modal.jsx";
+import { Link } from "react-scroll";
+import { Outlet } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false); //* Zustand für das Modal-Fenster
@@ -22,7 +24,7 @@ const Navbar = () => {
         <div className="container-fluid">
           <div className="navbar-brand ">
             <img
-              src="images/Logo/LogoRocadaro.png"
+              src="./public/images/Logo/LogoRocadaro.png"
               alt="Logo"
               className="logo"
             />
@@ -43,23 +45,25 @@ const Navbar = () => {
           >
             <ul className="navbar-nav mx-auto">
               <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Home
-                  <span className="visually-hidden">(current)</span>
+                <Link to="home" className="nav-link">
+                  <NavLink to="/" className="nav-link active">
+                    Home
+                    <span className="visually-hidden">(current)</span>
+                  </NavLink>
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/products" className="nav-link">
+                <Link to="products" className="nav-link">
                   Products
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/brands" className="nav-link">
+                <Link to="brands" className="nav-link">
                   Brands
                 </Link>
               </li>
               <li className="nav-item">
-                <Link to="/contact" className="nav-link">
+                <Link to="contact" className="nav-link">
                   Contact
                 </Link>
               </li>
@@ -78,12 +82,14 @@ const Navbar = () => {
             </form>
           </div>
         </div>
-
+        {/* {isModalOpen && <Modal onClose={handleModalToggle} />}{" "} */}
+        {/* Zeige das Modal-Fenster, wenn isModalOpen true ist */}
         <ModalComponents
           isModalOpen={isModalOpen}
           handleModalToggle={handleModalToggle}
         />
       </nav>
+      <Outlet />
     </>
   );
 };
