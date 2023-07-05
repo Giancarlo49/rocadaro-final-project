7884b6a7ae81db4fbe2cc18efae40331cf4ec9c9
RobertoRadina <robertoradina@gmail.com>
2023-07-05 10:49:26 +0200

    Create User Button

--- rocadaro/frontend/src/api.js
+++ rocadaro/frontend/src/api.js
@@ -1,24 +1,25 @@
 //1. Hier alle API dass wir brauchen
 
-export const loginApi = async(data) => {
-    // TODO: Implementierung der APIs
-    console.log("hello von login");
-    try {
-        const response = await fetch("http://localhost:5032/user/login",{
-            method: "POST",
-            body: JSON.stringify(data),
-            headers:{
-                "Content-Type":"application/json"
-            },
-            credentials: "include"
-        });
-        const userData = await response.json();
-        if (response.status === 200){
-            console.log("Anmeldung erfolgreich!");
-            return userData;
-        };
-        throw new Error("Anmeldung fehlgeschlagen!");
-    } catch (error) {
-        console.log(error);
+export const loginApi = async (data) => {
+  // TODO: Implementierung der APIs
+  console.log("hello von login");
+  try {
+    const response = await fetch("http://localhost:5032/user/login", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+    });
+    const userData = await response.json();
+    if (response.status === 200) {
+      console.log("Anmeldung erfolgreich!");
+      window.location.href = "/";
+      return userData;
     }
-}
+    throw new Error("Anmeldung fehlgeschlagen!");
+  } catch (error) {
+    console.log(error);
+  }
+};
