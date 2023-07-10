9f2d0cb9f503e86d896fc90e0b0b7d812b40bac1
David <davidcohnen@dg-email.de>
2023-07-10 17:09:50 +0200

    suchleiste vernüftig gemacht

--- rocadaro/frontend/src/pages/Home.jsx
+++ rocadaro/frontend/src/pages/Home.jsx
@@ -3,11 +3,11 @@ import "./Home.css";
 import Carousel from "../components/carousel/Carousel";
 import Products from "./Products";
 import Werbung from "../components/werbung/Werbung";
-import CreateUser from "../components/CreateUser";
+// import CreateUser from "../components/CreateUser";
 import Brands from "./Brands";
 import VariationsExample from "../components/footer/FooterButtons";
 import AnimationExample from "../components/footer/Endproject";
-import Searchbar from "../components/Seachbar";
+import Searchbar from "../components/searchbar/Searchbar";
 import Garantise from "../components/garantise/Garantise";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
@@ -15,12 +15,16 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const Home = () => {
   return (
     <section className="page" id="home">
-      {/* <Searchbar /> */}
+      
+      <Searchbar />
+      
       <Carousel />
-      <CreateUser />
+      {/* <CreateUser /> */}
+      
       <Products />
       <Werbung />
       <Brands />
+      
       <Garantise />
       <VariationsExample />
       <AnimationExample />
