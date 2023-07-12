2fdb4a17ae278af4906615e3331cf130b2ec3d5e
RoJiCal <roberto.jimenez@dci.education>
2023-07-12 10:20:21 +0200

    new

--- rocadaro/frontend/src/pages/Cart.jsx
+++ rocadaro/frontend/src/pages/Cart.jsx
@@ -4,39 +4,14 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import Cart from "../components/cart/ProductCart";
 import Store from "../store/Context";
 import "../pages/Cart.css";
-import { loadScript } from "@paypal/paypal-js";
-import { PayPalButton } from "react-paypal-button-v2";
+
 
 export default function ListCart() {
   const { show1, show2, toggleClose, cart } = Store();
 
   const [total, setTotal] = useState(0);
 
-  useEffect(() => {
-    const loadPayPalScript = async () => {
-      const scriptUrl = "https://www.paypal.com/sdk/js?client-id=AYKhvZPHAhnFbSwltpjUFq3IDOLFCu0hIba9AbQKBvWvjqOJnr19YfpDL6mlU2EdX0Q8P_Nos9NSQNam";
   
-      try {
-        await loadScript({ "url": scriptUrl });
-  
-        window.paypal.Buttons({
-          createOrder: function (data, actions) {
-            
-          },
-          onApprove: function (data, actions) {
-            
-          },
-          onError: function (err) {
-            
-          },
-        }).render("#paypal-button-container");
-      } catch (error) {
-        console.error("Error loading PayPal script:", error);
-      }
-    };
-  
-    loadPayPalScript();
-  }, []);
   
 
   useEffect(() => {
@@ -58,7 +33,7 @@ export default function ListCart() {
         ))}
         <div className="cart_footer">
           <h3>Total:{total}€</h3>
-          <PayPalButton/>
+          <button>Payment</button>
         </div>
       </div>
     </div>
