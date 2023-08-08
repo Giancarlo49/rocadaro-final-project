ad405a65cd397121fd336b0837c60e147cfdb7fe
RoJiCal <roberto.jimenez@dci.education>
2023-08-08 10:42:01 +0200

    footer

--- rocadaro/frontend/src/components/footer/Endproject.jsx
+++ rocadaro/frontend/src/components/footer/Endproject.jsx
@@ -3,11 +3,11 @@ import Placeholder from 'react-bootstrap/Placeholder';
 function AnimationExample() {
   return (
     <>
-      <Placeholder as="p" animation="glow">
+      <Placeholder as="p" animation="glow" style={{ backgroundColor: "white", color: '#09c2c2' }}>
         <Placeholder xs={12} />
       </Placeholder>
-      <p>End Project RoCaDaRo</p>
-      <Placeholder as="p" animation="wave">
+      <p>&copy; 2023 RoCaDaRo</p>
+      <Placeholder as="p" animation="wave" style={{ backgroundColor: "white", color: '#09c2c2' }}>
         <Placeholder xs={12} />
       </Placeholder>
     </>
